4fc914e240d2dbebadfad2675a94056cbf6d3943
huaweiwei <harvey0379@163.com>
2022-04-06 21:56:19 +0800

    useMemo

--- src/memo/Child.jsx
+++ src/memo/Child.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Child({ onClick }) {
-  console.log('child');
+function Child({ onClick, name, todoList = [] }) {
+  console.log('child', name);
   return (
     <fieldset>
-      <legend>child</legend>
+      <legend>{name}</legend>
+      todoList:{' '}
+      {todoList.map((item) => (
+        <span key={item.id}>{item.text}</span>
+      ))}
       <button onClick={onClick}> child's button</button>
     </fieldset>
   );
 }
 
 Child.propTypes = {
-  onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func,
+};
+
+Child.defaultProps = {
+  onClick: () => {
+    console.log('onClick');
+  },
 };
 
 export default React.memo(Child);
--- src/memo/Parent.jsx
+++ src/memo/Parent.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import Child from './Child';
 
 function Parent() {
@@ -7,13 +7,21 @@ function Parent() {
   const onClick = useCallback(() => {
     setCount((c) => c + 1);
   }, []);
+
+  const todoList = useMemo(() => [{ id: 1, text: 'react' }], []);
+
+  const onDouble = () => {
+    setCount((c) => c * 2);
+  };
   return (
     <fieldset>
       <legend>use memo</legend>
       {count}
       <button onClick={onClick}>parent btn</button>
 
-      <Child onClick={onClick} />
+      <Child onClick={onClick} name='useCallback' />
+      <Child onClick={onDouble} name='vanilla' />
+      <Child todoList={todoList} name='vanilla2' />
     </fieldset>
   );
 }

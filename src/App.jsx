e51a86f5005d9810173a1cb127f9a7430de7e3c1
huaweiwei <harvey0379@163.com>
2022-04-11 17:53:51 +0800

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react';
-import BasicComponent from './basic/BasicComponent';
-import { default as UseImperativeHandle } from './useImperativeHandle/Parent';
-import { default as MemoDemo } from './memo/Parent';
+import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import BasicComponent from './basic/BasicComponent'
+import styled from 'styled-components'
+import { default as UseImperativeHandle } from './useImperativeHandle/Parent'
+import { default as MemoDemo } from './memo/Parent'
 
+const StyledNav = styled('nav')`
+  width: 100%;
+  display: flex;
+  gap: 10px;
+`
 function App() {
   return (
-    <div>
-      <MemoDemo />
-      <UseImperativeHandle />
-      <BasicComponent />
-    </div>
-  );
+    <>
+      <StyledNav>
+        <Link to="/BasicComponent">BasicComponent</Link>
+        <Link to="/MemoDemo">MemoDemo</Link>
+        <Link to="/UseImperativeHandle">UseImperativeHandle</Link>
+      </StyledNav>
+      <Routes>
+        <Route path="BasicComponent" element={<BasicComponent />} />
+        <Route path="MemoDemo" element={<MemoDemo />} />
+        <Route path="UseImperativeHandle" element={<UseImperativeHandle />} />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App

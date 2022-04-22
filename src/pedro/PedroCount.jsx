a2e8eb6a68a700460943f268b2595e01a3a32096
huaweiwei <harvey0379@163.com>
2022-04-23 00:06:08 +0800

    closure

--- src/pedro/PedroCount.jsx
+++ src/pedro/PedroCount.jsx
@@ -3,12 +3,21 @@ import React, { useState } from 'react'
 function PedroCount() {
   const [count, setCount] = useState(0)
   const onClick = () => {
-    setCount((c) => c + 1)
+    setTimeout(() => {
+      setCount((c) => c + 1)
+    }, 1e3)
+  }
+
+  const onClick1 = () => {
+    setTimeout(() => {
+      setCount(count + 1)
+    }, 1e3)
   }
   return (
     <>
       {count}
-      <button onClick={onClick}>+</button>
+      <button onClick={onClick}>setCount((c) => c + 1)</button>
+      <button onClick={onClick1}>setCount(count + 1)</button>
     </>
   )
 }

f1a36a68af21ff18c6a7ac7bbfa4cf581cf1312b
huaweiwei <harvey0379@163.com>
2022-04-23 19:27:49 +0800

    intermediate

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,7 @@
-import { useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-import BasicComponent from './basic/BasicComponent'
-import styled from 'styled-components'
-import { default as UseImperativeHandle } from './useImperativeHandle/Parent'
-import { default as MemoDemo } from './memo/Parent'
-import HTMLCSSDemo from './demo/HTMLCSSDemo'
-import NeumorphismDemo from './pages/NeumorphismDemo'
-import PedroPage from './pedro/PedroPage'
+import React from 'react'
 
-const StyledNav = styled('nav')`
-  width: 100%;
-  display: flex;
-  gap: 10px;
-`
-function App() {
-  return (
-    <>
-      <StyledNav>
-        <Link to="/BasicComponent">BasicComponent</Link>
-        <Link to="/MemoDemo">MemoDemo</Link>
-        <Link to="/UseImperativeHandle">UseImperativeHandle</Link>
-        <Link to="/html-css">html css</Link>
-        <Link to="/Neumorphism">玻璃拟态</Link>
-        <Link to="/pedro">pedro</Link>
-      </StyledNav>
-      <Routes>
-        <Route path="BasicComponent" element={<BasicComponent />} />
-        <Route path="MemoDemo" element={<MemoDemo />} />
-        <Route path="UseImperativeHandle" element={<UseImperativeHandle />} />
-        <Route path="/html-css" element={<HTMLCSSDemo />} />
-        <Route path="/Neumorphism" element={<NeumorphismDemo />} />
-        <Route path="/pedro" element={<PedroPage />} />
-      </Routes>
-    </>
-  )
+const App = () => {
+  return <div>App</div>
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
-import theme from './components/styles/theme'
-import GlobalStyles from './components/styles/GlobalStyles'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles></GlobalStyles>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 )

7c4d2af28f2278a0426a177e6cbdade9c45d10c2
huaweiwei <harvey0379@163.com>
2022-05-14 00:06:44 +0800

    alias

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ export default () => {
   return defineConfig({
     base: './',
     plugins: [react()],
+    resolve: {
+      alias: [
+        {
+          find: '@/',
+          replacement: `${path.resolve(__dirname, 'src')}/`,
+        },
+      ],
+    },
     css: {
       preprocessorOptions: {
         less: {
